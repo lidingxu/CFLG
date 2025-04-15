8045849db2ad7699fa63bd4143eddecc54c63665
lxu <lxu@htc-login001.zib.de>
2025-04-15 07:49:20 +0200

    results

--- runjobs.sh
+++ runjobs.sh
@@ -5,6 +5,8 @@ if [ -d "outputs" ]; then
 fi
 lines=$(wc -l < job_list.txt)
 echo "Number of jobs: $lines"
+export GRB_LICENSE_FILE=/software/Gurobi/gurobi1201/gurobi.lic
+export LC_ALL=C
 export JULIA_DEPOT_PATH=".julia_depot"
 julia --project=. -e 'ENV["CPLEX_STUDIO_BINARIES"] = "/software/opt-sw/cplex/bin/x86-64_linux/"; using Pkg; Pkg.instantiate(); Pkg.precompile()'
 
