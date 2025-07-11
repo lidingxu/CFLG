dcbee402166a8ab17cc0df69bc7a3e3eb7b4111f
lxu <lxu@htc-login001.zib.de>
2025-07-11 09:20:56 +0200

    all results

--- jobs.sh
+++ jobs.sh
@@ -2,8 +2,8 @@
 # Set variables
 timelimit=7200
 timebound=7400
-formulations=("EF" "EFP" "LEFP" "LEVFP" "LEFPAD" "LEFPAV" "LEFPA" "LEFPAI") #("LEFPD" "LEFPV" "EF" "EFP" "LEFP" "LEFPI" "LEVFP" "None")
-covers=("Small")
+formulations=("EF" "EFP" "LEFP" "LEVFP" "LEFPAD" "LEFPAV" "LEFPA" "LEFPAI" "None") #("LEFPD" "LEFPV" "EF" "EFP" "LEFP" "LEFPI" "LEVFP" "None")
+covers=("Large")
 solver="Gurobi" # "Gurobi", "CPLEX", "GLPK", "SCIP".
 datapath="$PWD/benchmarks"
 resultpath="$PWD/results"
